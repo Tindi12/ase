f81dbf6d9062948a0dc1e26f4c3b3bbe89bfaf6a
gelzinyte <elena.gelzinyte@gmail.com>
2025-09-29 20:37:25 +0200

    CalculationResults mwe

---
+++ ase/_4/atoms.py
@@ -0,0 +1,38 @@
+from ase._4.calculators.results import CalculationResults
+from ase.atoms import Atoms as v3Atoms
+from ase.outputs import ArrayProperty, all_outputs
+
+
+class DummyAtoms(v3Atoms):
+    """
+    Dummy class to illustrate how `Atoms.store` works with
+    `CalculationResults`. Will be overwritten once this is
+    merged with the branch that implements the updated
+    Atoms interface.
+    """
+
+    def __init__(self, kwargs) -> None:
+        super.__init__(**kwargs)
+
+    def store(self, results: CalculationResults, label: str = '') -> None:
+        """
+        Stores properties from CalculationResults in Atoms.info and
+        Atoms.arrays under "labelproperty".
+        """
+        properties = results.properties
+        if len(properties) == 0:
+            raise ValueError(
+                'The CalculationResults instance has no properties to save'
+            )
+
+        # All properties in CalculationResults are compatible with
+        # `ase.outputs.all_outputs.
+        for prop_name, prop_val in properties.items():
+            output_type = all_outputs[prop_name]
+            if (
+                isinstance(output_type, ArrayProperty)
+                and output_type.shapespec[0] == 'natoms'
+            ):
+                self.arrays[label + prop_name] = prop_val
+            else:
+                self.info[label + prop_name] = prop_val
---
+++ ase/_4/test/calculator/test_results.py
@@ -0,0 +1,84 @@
+import numpy as np
+import pytest
+
+from ase._4.calculators.results import CalculationResults
+
+
+def test_data_at_initialisation(metadata, properties):
+    # Data assigned at initialisation
+    res = CalculationResults(metadata=metadata, properties=properties)
+    validate(res)
+
+
+def test_data_after_initialisation(metadata, properties):
+    # after initialisation
+    res = CalculationResults()
+    res.metadata = metadata
+    res.properties = properties
+    validate(res)
+
+
+def test_add_single_property(metadata, properties):
+    # initialise and add single property
+    res = CalculationResults()
+    res.add_property('energy', 3.14)
+    validate(res)
+
+    # test immutability/assignment
+    with pytest.raises(ValueError):
+        res.add_property('energy', 4.32)
+
+
+def test_wrong_dtype():
+    # shouldn't allow non-dict or non-Properties
+    res = CalculationResults()
+    with pytest.raises(TypeError):
+        res.metadata = 'test'
+    with pytest.raises(TypeError):
+        res.properties = 'test'
+
+
+def test_no_overwriting(metadata, properties):
+    # shouldn't allow overwriting
+    res = CalculationResults(metadata=metadata, properties=properties)
+    with pytest.raises(AttributeError):
+        res.metadata = metadata
+    with pytest.raises(AttributeError):
+        res.properties = properties
+
+
+def test_add_individual_metadata():
+    # try setting individual metadata and properties
+    res = CalculationResults()
+    res.add_metadata('calculator_name', 'test')
+    with pytest.raises(AttributeError):
+        res.add_metadata('calculator_name', 'test2')
+
+
+def test_add_individual_properties(properties):
+    res = CalculationResults()
+    # properties hasn't been set, should work
+    res.properties = properties
+    # now energy is set, should raise
+    with pytest.raises(ValueError):
+        res.add_property('energy', 1.62)
+
+
+def test_add_unrecognised_property():
+    res = CalculationResults()
+    with pytest.raises(KeyError):
+        res.add_property('unrecognised_energy', 1.62)
+
+
+def test_add_wrong_property_shape():
+    res = CalculationResults()
+    # try to set the wrong shape
+    with pytest.raises(ValueError):
+        res.add_property('forces', np.arange(5))
+    # try to set the wrong datatype
+    with pytest.raises(TypeError):
+        res.add_property('energy', np.arange(5))
+
+
+def validate(res):
+    pass
