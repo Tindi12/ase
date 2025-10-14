d8d481147e32541b931963c42c558dfc12eda00d
James Kermode <james.kermode@gmail.com>
2025-10-14 12:25:53 +0100

    Fix more formatting problems

--- ase/_4/atoms.py
+++ ase/_4/atoms.py
@@ -23,7 +23,7 @@ class Atoms(v3Atoms):
             pbc=v3atoms.pbc,
         )
 
-    def store(self, results: CalculationResults, label: str = "") -> None:
+    def store(self, results: CalculationResults, label: str = '') -> None:
         """
         Stores properties from CalculationResults in Atoms.info and
         Atoms.arrays under "labelproperty".
@@ -31,7 +31,7 @@ class Atoms(v3Atoms):
         properties = results.properties
         if len(properties) == 0:
             raise ValueError(
-                "The CalculationResults instance has no properties to save"
+                'The CalculationResults instance has no properties to save'
             )
 
         # All properties in CalculationResults are compatible with
@@ -40,41 +40,44 @@ class Atoms(v3Atoms):
             output_type = all_outputs[prop_name]
             if (
                 isinstance(output_type, ArrayProperty)
-                and output_type.shapespec[0] == "natoms"
+                and output_type.shapespec[0] == 'natoms'
             ):
                 self.arrays[label + prop_name] = prop_val
             else:
                 self.info[label + prop_name] = prop_val
 
-    def get_potential_energy(self, force_consistent=False,
-                             apply_constraint=True):
-        raise NotImplementedError("moved to Calculator.evalute() in ASEv4")
+    def get_potential_energy(
+        self, force_consistent=False, apply_constraint=True
+    ):
+        raise NotImplementedError('moved to Calculator.evalute() in ASEv4')
 
-    def get_potential_energies(self, force_consistent=False,
-                               apply_constraint=True):
-        raise NotImplementedError("moved to Calculator.evalute() in ASEv4")
+    def get_potential_energies(
+        self, force_consistent=False, apply_constraint=True
+    ):
+        raise NotImplementedError('moved to Calculator.evalute() in ASEv4')
 
     def get_total_energy(self):
-        raise NotImplementedError("moved to Calculator.evalute() in ASEv4")
+        raise NotImplementedError('moved to Calculator.evalute() in ASEv4')
 
     def get_forces(self, apply_constraint=True, md=False):
-        raise NotImplementedError("moved to Calculator.evalute() in ASEv4")
+        raise NotImplementedError('moved to Calculator.evalute() in ASEv4')
 
-    def get_stress(self, voigt=True, apply_constraint=True,
-                   include_ideal_gas=False):
-        raise NotImplementedError("moved to Calculator.evalute() in ASEv4")
+    def get_stress(
+        self, voigt=True, apply_constraint=True, include_ideal_gas=False
+    ):
+        raise NotImplementedError('moved to Calculator.evalute() in ASEv4')
 
     def get_stresses(self, include_ideal_gas=False, voigt=True):
-        raise NotImplementedError("moved to Calculator.evalute() in ASEv4")
+        raise NotImplementedError('moved to Calculator.evalute() in ASEv4')
 
     @property
     def calc(self):
-        raise NotImplementedError("Calculator no longer lives in Atoms")
+        raise NotImplementedError('Calculator no longer lives in Atoms')
 
     @calc.setter
     def calc(self, calc):
-        raise NotImplementedError("Calculator no longer lives in Atoms")
+        raise NotImplementedError('Calculator no longer lives in Atoms')
 
     @calc.deleter
     def calc(self):
-        raise NotImplementedError("Calculator no longer lives in Atoms")
+        raise NotImplementedError('Calculator no longer lives in Atoms')
