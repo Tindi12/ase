2f63d6176865820af50a82d1c61e9fef37451fa7
gelzinyte <elena.gelzinyte@gmail.com>
2025-10-14 11:25:49 +0200

    make asev4 tests non-optional and part of main gitlab ci

--- ase/test/_4/conftest.py
+++ ase/test/_4/conftest.py
@@ -9,17 +9,6 @@ from ase.build import bulk
 from ase.outputs import Properties
 
 
-def pytest_collection_modifyitems(config, items):
-    """By default skips tests with "asev4" mark."""
-    run_v4 = config.option.markexpr and 'asev4' in config.option.markexpr
-    skip_marker = pytest.mark.skip(reason="ASEv4 test, run with '-m asev4'")
-
-    for item in items:
-        if '_4' in str(item.fspath):
-            if not run_v4:
-                item.add_marker(skip_marker)
-
-
 @pytest.fixture
 def atoms() -> V3Atoms:
     return bulk('Cu', 'fcc', a=3.6)
