596c7c07c13f2c86aebf13671fee46e15f3f0d8f
James Kermode <james.kermode@gmail.com>
2025-10-14 11:44:42 +0100

    Disable type checking on test that uses intentionally incorrect types

--- ase/test/_4/test_results.py
+++ ase/test/_4/test_results.py
@@ -35,9 +35,9 @@ def test_wrong_dtype():
     # shouldn't allow non-dict or non-Properties
     res = CalculationResults()
     with pytest.raises(TypeError):
-        res.metadata = 'test'
+        res.metadata = 'test'    # type: ignore
     with pytest.raises(TypeError):
-        res.properties = 'test'
+        res.properties = 'test'  # type: ignore
 
 
 def test_no_overwriting(metadata, properties):
