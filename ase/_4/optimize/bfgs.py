6eec0aa597a802a04c6568f1a3cad2109ab67bfb
Ask Hjorth Larsen <asklarsen@gmail.com>
2026-01-09 11:05:48 +0100

    separate out the tests

--- ase/_4/optimize/bfgs.py
+++ ase/_4/optimize/bfgs.py
@@ -2,11 +2,12 @@ import json
 from dataclasses import asdict, dataclass
 
 import numpy as np
-import pytest
 
 from ase._4.optimize.cellutil import CellUtility
 from ase.io.jsonio import (
     default as jsonio_default,
+)
+from ase.io.jsonio import (
     object_hook as jsonio_object_hook,
 )
 from ase.io.trajectory import Trajectory
@@ -50,20 +51,6 @@ class BFGSMethod:
         n = int(np.round(len(hessian) ** 0.5))
         hessian = np.array(hessian).reshape(n, n)
         return cls(hessian)
-
-
-def setup_surface():
-    from ase.build import fcc111
-    from ase.calculators.emt import EMT
-
-    rng = np.random.RandomState(42)
-    atoms = fcc111('Au', size=(1, 2, 2), vacuum=5.0)
-    atoms.rattle(stdev=0.01, rng=rng)
-    cell = atoms.get_cell()
-    cell[:2, :2] += 0.05 * rng.random((2, 2))
-    atoms.set_cell(cell, scale_atoms=True)
-    atoms.calc = EMT()
-    return atoms
 
 
 class Target:
@@ -368,6 +355,8 @@ def irun(target, method, step):
 
 def next_step(target, method, step) -> Step:
     dx = method.compute_step(step.gradient_obj.gradient)
+    # We do not have maxstep right now.  This will not run the same
+    # as legacy optimizations until we apply a maxstep.
 
     # Target may apply constraints or other magic, so we may not
     # get the same x back as the one we set.
@@ -389,41 +378,6 @@ def next_step(target, method, step) -> Step:
     return newstep
 
 
-def test_surface():
-    from ase.optimize.bfgs import BFGS as OldBFGS
-
-    atoms = setup_surface()
-
-    bfgs = OldBFGS(atoms)
-    for _ in bfgs.irun(fmax=0.01):
-        pass
-    # bfgs.run(fmax=0.01)
-
-
-def test_new_bfgs():
-    atoms = setup_surface()
-    new_bfgs(Target(atoms, fmax=0.01), initial_position_hessian(3 * len(atoms)))
-
-
-def test_old_frechet():
-    print('OLD FRECHET')
-    from ase.filters import FrechetCellFilter
-    from ase.optimize.cellawarebfgs import CellAwareBFGS
-
-    atoms = setup_surface()
-    bfgs = CellAwareBFGS(
-        FrechetCellFilter(atoms, exp_cell_factor=1.0, mask=[1, 1, 0, 0, 0, 1])
-    )
-    bfgs.run(fmax=0.001, smax=0.0001)
-
-
-def test_new_bfgs_frechet():
-    atoms = setup_surface()
-    target = FrechetTarget(atoms, fmax=0.01, smax=0.001)
-    hessian = target.initial_hessian()
-    new_bfgs(target=target, hessian=hessian)
-
-
 def write_to_log(method, log, step):
     loginfo = step.gradient_obj.loginfo()
     name = method.methodname
@@ -436,81 +390,6 @@ def write_to_traj(target, trajpath, comm):
     with Trajectory(trajpath, comm=comm, mode='a') as traj:
         # XXX we are not setting metadata (like old optimizers)
         traj.write(target)
-
-
-def test_new_bfgs_frechet_files(tmp_path):
-    from ase.optimize.optimize import Log
-    from ase.parallel import world
-
-    comm = world
-
-    atoms = setup_surface()
-    fmax = 0.001
-    smax = 0.0001
-    target = FrechetTarget(atoms, fmax=fmax, smax=smax)
-    hessian = target.initial_hessian()
-    method = BFGSMethod(hessian)
-
-    log = Log('-', comm)
-    restartpath = tmp_path / 'restart.json'
-    trajpath = tmp_path / 'opt.traj'
-    trajpath.unlink(missing_ok=True)
-
-    def writefiles():
-        write_to_log(method, log, step)
-        write_to_traj(target, trajpath, comm)
-        write_restartfile(restartpath, method, target, step)
-
-    step = Step.start(target)
-    writefiles()
-
-    for step in irun(target, method, step):
-        writefiles()
-        if step.i == 5:
-            break
-
-    firstpart_images = read_images(trajpath)
-
-    assert len(firstpart_images) == 6
-
-    halfway = restartpath.with_name('halfway.json')
-    write_restartfile(halfway, method, target, step)
-
-    for step in irun(target, method, step):
-        writefiles()
-
-    gradient_obj = step.gradient_obj
-
-    ref = pytest.approx(0.837190)
-
-    assert target.get_value() == ref
-    assert gradient_obj.fnorm < fmax
-    assert gradient_obj.snorm < smax
-    assert step.i == 17
-
-    images = read_images(trajpath)
-    last_atoms = images[-1]
-
-    assert len(images) == 18
-    assert last_atoms.get_potential_energy() == ref
-
-    from ase.calculators.emt import EMT
-
-    print('restart')
-    target, method, step = read_restartfile(halfway, EMT())
-
-    trajpath = tmp_path / 'lastpath.traj'
-    write_to_log(method, log, step)
-
-    for step in irun(target, method, step):
-        writefiles()
-
-    lastpart_images = read_images(trajpath)
-    assert len(firstpart_images) + len(lastpart_images) == len(images)
-    last_atoms2 = lastpart_images[-1]
-    assert last_atoms2.get_potential_energy() == pytest.approx(ref)
-    assert last_atoms.positions == pytest.approx(last_atoms2.positions)
-    assert last_atoms.cell == pytest.approx(last_atoms2.cell)
 
 
 def read_images(trajpath):
---
+++ ase/test/ase4/test_optimize.py
@@ -0,0 +1,139 @@
+import numpy as np
+import pytest
+
+from ase._4.optimize.bfgs import (
+    BFGSMethod,
+    FrechetTarget,
+    Step,
+    Target,
+    irun,
+    new_bfgs,
+    read_images,
+    read_restartfile,
+    write_restartfile,
+    write_to_log,
+    write_to_traj,
+)
+from ase.filters import FrechetCellFilter
+from ase.optimize.bfgs import BFGS as OldBFGS
+from ase.optimize.cellawarebfgs import CellAwareBFGS
+from ase.optimize.optimize import Log
+from ase.parallel import world
+
+
+def setup_surface():
+    from ase.build import fcc111
+    from ase.calculators.emt import EMT
+
+    rng = np.random.RandomState(42)
+    atoms = fcc111('Au', size=(1, 2, 2), vacuum=5.0)
+    atoms.rattle(stdev=0.01, rng=rng)
+    cell = atoms.get_cell()
+    cell[:2, :2] += 0.05 * rng.random((2, 2))
+    atoms.set_cell(cell, scale_atoms=True)
+    atoms.calc = EMT()
+    return atoms
+
+
+def test_surface():
+    atoms = setup_surface()
+
+    bfgs = OldBFGS(atoms)
+    for _ in bfgs.irun(fmax=0.01):
+        pass
+
+
+def test_new_bfgs():
+    atoms = setup_surface()
+    target = Target(atoms, fmax=0.01)
+    hessian = target.initial_hessian()
+    new_bfgs(target, hessian)
+
+
+def test_old_frechet():
+    print('OLD FRECHET')
+
+    atoms = setup_surface()
+    bfgs = CellAwareBFGS(
+        FrechetCellFilter(atoms, exp_cell_factor=1.0, mask=[1, 1, 0, 0, 0, 1])
+    )
+    bfgs.run(fmax=0.001, smax=0.0001)
+
+
+def test_new_bfgs_frechet():
+    atoms = setup_surface()
+    target = FrechetTarget(atoms, fmax=0.01, smax=0.001)
+    hessian = target.initial_hessian()
+    new_bfgs(target=target, hessian=hessian)
+
+
+def test_new_bfgs_frechet_files(tmp_path):
+    comm = world
+
+    atoms = setup_surface()
+    fmax = 0.001
+    smax = 0.0001
+    target = FrechetTarget(atoms, fmax=fmax, smax=smax)
+    hessian = target.initial_hessian()
+    method = BFGSMethod(hessian)
+
+    log = Log('-', comm)
+    restartpath = tmp_path / 'restart.json'
+    trajpath = tmp_path / 'opt.traj'
+    trajpath.unlink(missing_ok=True)
+
+    def writefiles():
+        write_to_log(method, log, step)
+        write_to_traj(target, trajpath, comm)
+        write_restartfile(restartpath, method, target, step)
+
+    step = Step.start(target)
+    writefiles()
+
+    for step in irun(target, method, step):
+        writefiles()
+        if step.i == 5:
+            break
+
+    firstpart_images = read_images(trajpath)
+
+    assert len(firstpart_images) == 6
+
+    halfway = restartpath.with_name('halfway.json')
+    write_restartfile(halfway, method, target, step)
+
+    for step in irun(target, method, step):
+        writefiles()
+
+    gradient_obj = step.gradient_obj
+
+    ref = pytest.approx(0.837190)
+
+    assert target.get_value() == ref
+    assert gradient_obj.fnorm < fmax
+    assert gradient_obj.snorm < smax
+    assert step.i == 17
+
+    images = read_images(trajpath)
+    last_atoms = images[-1]
+
+    assert len(images) == 18
+    assert last_atoms.get_potential_energy() == ref
+
+    from ase.calculators.emt import EMT
+
+    print('restart')
+    target, method, step = read_restartfile(halfway, EMT())
+
+    trajpath = tmp_path / 'lastpath.traj'
+    write_to_log(method, log, step)
+
+    for step in irun(target, method, step):
+        writefiles()
+
+    lastpart_images = read_images(trajpath)
+    assert len(firstpart_images) + len(lastpart_images) == len(images)
+    last_atoms2 = lastpart_images[-1]
+    assert last_atoms2.get_potential_energy() == pytest.approx(ref)
+    assert last_atoms.positions == pytest.approx(last_atoms2.positions)
+    assert last_atoms.cell == pytest.approx(last_atoms2.cell)
