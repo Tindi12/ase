152ee5b70c782057b0a02c9b3ff87587ebf4b067
Mikael Kuisma <kuisma@dtu.dk>
2026-02-20 12:59:39 +0100

    [PATCH 03/34] Converges on a simple system

--- ase/_4/symopt/relax.py
+++ ase/_4/symopt/relax.py
@@ -15,7 +15,7 @@ def pretty(C_cv):
 def chol_derivative(A, dA):
     L = np.linalg.cholesky(A)
     Lp = np.linalg.cholesky(A + dA)
-    return Lp - L
+    return (Lp - L) / np.linalg.norm(dA)
 
     # L lower-triangular Cholesky of A
     Linv = np.linalg.inv(L)
@@ -55,10 +55,11 @@ def unit_cell_symmetry(C_cv, U_scc):
     rot_vv, P_vv = scipy.linalg.polar(F_vv)
     osymC_cv = symC_cv @ rot_vv.T
     print("Old cell like, but symmetrized", osymC_cv)
-
+    print(Atoms(cell=osymC_cv).cell.angles())
+    print(Atoms(cell=osymC_cv).cell.lengths())
     # Now we can construct exact Cartesian rotation matrices
-    iosymC_cv = np.linalg.inv(osymC_cv).T
-    U_svv = np.array([iosymC_cv @ U_cc @ osymC_cv for U_cc in U_scc])
+    iosymC_cv = np.linalg.inv(osymC_cv)
+    U_svv = np.array([osymC_cv.T @ U_cc.T @ iosymC_cv.T for U_cc in U_scc])
 
     # Build unit vector in symmetric matrix space
     def e(i, j):
@@ -75,24 +76,21 @@ def unit_cell_symmetry(C_cv, U_scc):
                 rows.append((U_vv @ e(i, j) @ U_vv.T - e(i, j)).reshape((9,)))
         A_blocks.append(np.vstack(rows))
     A = np.vstack(A_blocks)
-    print(A)
     # Compute null space via SVD
     U, S, Vh = np.linalg.svd(A)
-    print(S)
     tol = 1e-6
     null_mask = S < tol
     nullspace = Vh[null_mask]
-    print(nullspace)
     dM_zcc = []
     for B in nullspace:
-        dof = osymC_cv.T @ B.reshape((3, 3)) @ osymC_cv
+        dof = osymC_cv @ B.reshape((3, 3)) @ osymC_cv.T
         dM_zcc.append(dof)
-    dM_zcc = np.array(dM_zcc)
+    dM_zcc = np.array(dM_zcc).reshape((-1, 3, 3))
 
     # Do a QR decomposition, try to get more zeros to coordinates
-    basis = np.array(dM_zcc).reshape((-1, 9))
-    Q, R = np.linalg.qr(basis)
-    dM_zcc = (Q.T @ basis).reshape((-1, 3, 3))
+    # basis = np.array(dM_zcc).reshape((-1, 9))
+    # Q, R = np.linalg.qr(basis)
+    # dM_zcc = (Q.T @ basis).reshape((-1, 3, 3))
 
     symC_cv = np.linalg.cholesky(M_cc)
 
@@ -103,7 +101,7 @@ def unit_cell_symmetry(C_cv, U_scc):
         print("C_cv:")
         pretty(chol_derivative(M_cc, 1e-5 * dM_cc) @ rot_vv.T)
 
-    return osymC_cv, U_svv, np.array(dM_zcc)
+    return M_cc, osymC_cv, U_svv, np.array(dM_zcc), rot_vv
 
 
 class Relax:
@@ -111,8 +109,8 @@ class Relax:
         if atoms.calc is not None:
             raise ValueError("Do not attach a calculator to Atoms yet.")
 
-        if not isinstance(calc, ASECalculator):
-            raise ValueError("Calculator must be new GPAW.")
+        # if not isinstance(calc, ASECalculator):
+        #    raise ValueError("Calculator must be new GPAW.")
 
         self.atoms = atoms
         self.calc = calc
@@ -121,17 +119,19 @@ class Relax:
 
         self.symmetries = create_symmetries_object(self.atoms, tolerance=self.symprec)
 
-        self.C_cv, U_svv, self.dM_zcc = unit_cell_symmetry(
+        self.M_cc, self.C_cv, U_svv, self.dM_zcc, self.rot_vv = unit_cell_symmetry(
             self.atoms.cell, self.symmetries.rotation_scc
         )
 
-        self.atoms.set_cell(C_cv, scale_atoms=True)
+        self.atoms.set_cell(self.C_cv, scale_atoms=True)
         # Now, with cell (and later atoms) symmetrized, it is safe to assign the calculator
         self.atoms.calc = calc
 
-        self._ndofs = len(dM_zcc)
+        self._ndofs = len(self.dM_zcc)
 
         self.optimizer = optimizer_factory(self)
+
+        self.value_z = np.zeros((self._ndofs))
 
     def run(self, *, fmax, smax):
         self.optimizer.run(fmax=fmax)
@@ -139,14 +139,67 @@ class Relax:
     def __ase_optimizable__(self):
         return self
 
+    def get_value(self):
+        return self.atoms.get_potential_energy()
+
+    def get_x(self):
+        return self.value_z.copy()
+
+    def _get_cell(self):
+        M_cc = self.M_cc + np.einsum("z,zcd->cd", self.value_z, self.dM_zcc)
+        C_cv = np.linalg.cholesky(M_cc) @ self.rot_vv.T
+        return Atoms(cell=C_cv).cell
+
+    def set_x(self, x):
+        self.value_z[:] = x
+        # print('z values', self.value_z)
+        M_cc = self.M_cc + np.einsum("z,zcd->cd", self.value_z, self.dM_zcc)
+        C_cv = np.linalg.cholesky(M_cc) @ self.rot_vv.T
+        # print('Current cell', C_cv)
+        # print('Volume', np.linalg.det(C_cv))
+        self.atoms.set_cell(C_cv, scale_atoms=True)
+
     def get_gradient(self):
+        eps = 1e-5
+        xref = np.array(self.get_x())
         grad_z = np.zeros((self.ndofs(),))
-        S_vv = self.atoms.get_stress(voight=False)
+        for z in range(self.ndofs()):
+            x = xref.copy()
+            x[z] += eps
+            self.set_x(x)
+            E1 = self.get_value()
+            x[z] -= 2 * eps
+            self.set_x(x)
+            E0 = self.get_value()
+
+            grad_z[z] = (E1 - E0) / (2 * eps)
+        self.set_x(xref)
+        return grad_z
+        # Infinitesimal deformation gradient
+        # F = chol(M_cc + \sum_z u_z dM^z_cc) chol(M_cc)^-1 = I + dF
+        # Strain eps = 1/2 (F^T F -I)
+        # Thus, d eps = 1/2 (dF + dF.T)
+        # dE/du_z = dE/deps_vv deps_vv
+
+        # dE = dE/deps_vv
+        grad_z = np.zeros((self.ndofs(),))
+        S_vv = self.atoms.get_stress(voigt=False)
         iC_cv = np.linalg.inv(self.C_cv)
         for z in range(self.ndofs()):
             dC_vv = 0.5 * iC_cv.T @ self.dM_zcc[z] @ iC_cv
             grad_z[z] = np.sum(np.sum(dC_vv * S_vv))
+        # print('gradient value', grad_z)
+        # print('S_vv', S_vv)
         return grad_z
+
+    def converged(self, gradient, fmax):
+        cell = self._get_cell()
+        print(cell.lengths(), cell.angles())
+        return self.gradient_norm(gradient) < fmax
+
+    def gradient_norm(self, grad_z):
+        # Go actually to cell metric
+        return np.max(np.abs(grad_z))
 
     def ndofs(self):
         return self._ndofs
@@ -154,13 +207,31 @@ class Relax:
 
 if __name__ == "__main__":
     from ase.build import bulk
+    from ase.calculators.emt import EMT
 
-    atoms = bulk("Au")
-    eps_cc = np.random.rand(3, 3) * 0.000
-    atoms.set_cell(atoms.cell @ (np.eye(3) + eps_cc), scale_atoms=True)
-    calc = GPAW(mode="pw")
+    atoms = bulk("Zn")
+    for atom in atoms:
+        atom.symbol = "Au"
+    # eps_cc = np.random.rand(3, 3) * 0.0001
+    # atoms.set_cell(atoms.cell @ (np.eye(3) + eps_cc), scale_atoms=True)
+    # calc = GPAW(mode="pw", txt='out.txt', convergece={'density':1e-7})
+
+    calc = EMT()
     print(type(calc))
     from ase.optimize.bfgs import BFGS
 
     relax = Relax(atoms=atoms, calc=calc, optimizer_factory=BFGS, symprec=1e-1)
+    if 0:
+        for z in range(2):
+            vec = np.zeros((2,))
+            relax.set_x(vec)
+            E0 = relax.get_value()
+            grad = relax.get_gradient()
+            vec[z] = 1e-3
+            relax.set_x(vec)
+            E1 = relax.get_value()
+            print("Finite difference grad", (E1 - E0) / 1e-3)
+            print("Gotten grad", grad[z])
+            print("div", grad[z] / ((E1 - E0) / 1e-3))
+
     relax.run(fmax=0.005, smax=0.001)
